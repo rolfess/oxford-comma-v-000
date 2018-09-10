40fa1e3813c38ed244026cf996b93a083b37efa4
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-10 21:59:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,10 @@
 def oxford_comma(array)
   #log and remove last element of the array
   last_element = array.last
+
   array.pop
   string1 = array.join(", ")
-  string1 = string1 + ", and "
+  string1 = string1 + " and "
   string1 = string1 + last_element
 
 
