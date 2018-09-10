e78b42277f664b84cfbb8b9bb5894122624d197f
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-10 22:32:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  #log and remove last element of the array
+
+  last_element = array.last
+  if array.size < 2
+    string1 = array.join
+  elsif array.size < 3
+    array.pop
+    string1 = array.join
+    string1 = string1 + " and " + last_element
+  else
+    array.pop
+    string1 = array.join (", ")
+    string1 = string1 + ", and " + last_element
+  end
 
 end
