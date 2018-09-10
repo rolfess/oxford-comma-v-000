e1f02580b71d03e994ab592c605f59b9b82dc6c1
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-10 22:29:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,14 +2,12 @@ def oxford_comma(array)
   #log and remove last element of the array
 
   last_element = array.last
-  array.pop
-  print array.size
   if array.size < 2
     string1 = array.join
   elsif array.size < 3
     array.pop
-    string1 = array.join(" and ")
-    string1 = string1 + last_element
+    string1 = array.join
+    string1 = string1 + " and " + last_element
   else
     string1 = array.join(", ")
     string1 = string1 + " and "
