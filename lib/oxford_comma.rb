012f12b5637de9475faac5d64e58068e8b7c8fef
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-10 19:11:48 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,8 @@
 def oxford_comma(array)
-    size = array.size - 1
-  if array.size > 2
-    #combines 3 or more items
-     last_element = array[size]
-     puts last_element
-     array.pop
-     array.join (", ")
-     array << "and"
-     array << last_element
-   elsif array.size > 1
-     last_element = array.last
-     array.pop
-     array.join (" and ")
-   end
-
+  #log and remove last element of the array
+  last_element = array.last
+  array.pop
+  string1 = array.join(",")
+  
+  
 end
